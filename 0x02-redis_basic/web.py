0831a0300f3e79959bfd3a4534d0abd101f9bde8
BouChrahns99 <bouchrahannnasi5@gmail.com>
2024-04-30 08:51:49 -0700

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
+""" Main file """
 
-import redis
-import requests
-from typing import Callable
-from functools import wraps
+Cache = __import__("exercise").Cache
 
-redis = redis.Redis()
+cache = Cache()
 
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
 
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
-
-    return wrapper
-
-
-@wrap_requests
-def get_page(url: str) -> str:
-    """get page self descriptive
-    """
-    response = requests.get(url)
-    return response.text
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
